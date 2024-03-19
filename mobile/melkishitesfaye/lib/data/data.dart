b6211711f15bf298f9b8fd86cc925d42142362fd
melki <melki@gmail.com>
2024-03-19 23:33:25 +0300

    [Mobile]Add Static data

---
+++ mobile/melkishitesfaye/lib/data/data.dart
@@ -0,0 +1,63 @@
+import 'package:melkishitesfaye/model/product.dart';
+
+Product product1 = Product(
+  id: '1',
+  imageUrl: 'assets/images/imges.jpeg',
+  price: '113',
+  productType: 'mens',
+  rating: '4.0',
+  productName: 'product1',
+);
+
+Product product2 = Product(
+  id: '2',
+  imageUrl: 'assets/images/imges.jpeg',
+  price: '223',
+  productType: 'mens',
+  rating: '4.0',
+  productName: 'product2',
+);
+
+Product product5 = Product(
+  id: '5',
+  imageUrl: 'assets/images/imges.jpeg',
+  price: '23',
+  productType: 'mens',
+  rating: '3.0',
+  productName: 'product5',
+);
+
+Product product3 = Product(
+  id: '3',
+  imageUrl: 'assets/images/imges.jpeg',
+  price: '123',
+  productType: 'womens',
+  rating: '3.5',
+  productName: 'product3',
+);
+
+Product product4 = Product(
+  id: '4',
+  imageUrl: 'assets/images/imges.jpeg',
+  price: '123',
+  productType: 'mens',
+  rating: '3.0',
+  productName: 'product4',
+);
+
+List<Product> products = [
+  product1,
+  product2,
+  product3,
+  product4,
+  product5,
+];
+
+// Map<String, dynamic> products = {
+//   'product1': product1,
+//   'product2': product2,
+//   'prroduct3': product3,
+//   'product4': product4,
+//   'product5': product5
+// };
+
